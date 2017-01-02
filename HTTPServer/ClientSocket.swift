4fbda1ac06e5d7edf19b0d6233d9c87fdc99aa07
Florian Kugler <mail@floriankugler.com>
2017-01-02 17:25:44 +0100

    Clean up ClientSocket

--- HTTPServer/ClientSocket.swift
+++ HTTPServer/ClientSocket.swift
@@ -10,36 +10,27 @@ import Foundation
 import SocketHelpers
 
 
-
-public struct SocketAddress {
-    /// Wraps a `sockaddr`, but could have more data than `sizeof(sockaddr)`
-    let data: Data
-    
-    init(data: Data) {
-        self.data = data
-    }
-}
-
-
-
 /// A socket that connects to a client, i.e. a program that connected to us.
-struct ClientSocket {
+final class ClientSocket {
     let address: SocketAddress
     fileprivate let backingSocket: CInt
+    
     init(address: SocketAddress, backingSocket: CInt) {
         self.address = address
         self.backingSocket = backingSocket
     }
-}
 
-
-extension ClientSocket {
-    /// Creates a dispatch I/O channel associated with the socket.
     func createIOChannel(with queue: DispatchQueue) -> DispatchIO {
         return DispatchIO(type: .stream, fileDescriptor: backingSocket, queue: queue) { error in
             print("Error on socket: \(error)")
         }
     }
+}
+
+
+public struct SocketAddress {
+    /// Wraps a `sockaddr`, but could have more data than `sizeof(sockaddr)`
+    let data: Data
 }
 
 
@@ -59,36 +50,33 @@ extension SocketAddress : CustomStringConvertible {
     }
 }
 
+
 extension SocketAddress {
     fileprivate var inFamily: sa_family_t {
-        let pointer = (data as NSData).bytes.bindMemory(to: sockaddr_in.self, capacity: data.count)
-        return pointer.pointee.sin_family
+        return data.withUnsafeBytes { (bytes: UnsafePointer<sockaddr_in>) in
+            bytes.pointee.sin_family
+        }
     }
     
     fileprivate var inAddrDescription: String? {
-        let pointer = (data as NSData).bytes.bindMemory(to: sockaddr_in.self, capacity: data.count)
-        switch inFamily {
-        case sa_family_t(AF_INET6):
-            fallthrough
-        case sa_family_t(AF_INET):
-            let data = NSMutableData(length: Int(INET6_ADDRSTRLEN))!
-            let inAddr = (UnsafeRawPointer(pointer) + offsetOf__sin_addr__in__sockaddr_in())
-            let dst = data.mutableBytes.assumingMemoryBound(to: Int8.self)
-            if inet_ntop(AF_INET, inAddr, dst, socklen_t(data.length)) != nil {
-                return (NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue)! as String)                }
-            return nil
-        default:
-            return nil
+        return data.withUnsafeBytes { (dataPtr: UnsafePointer<sockaddr_in>) in
+            guard [sa_family_t(AF_INET6), sa_family_t(AF_INET)].contains(inFamily) else { return nil }
+            var descriptionData = Data(capacity: Int(INET6_ADDRSTRLEN))
+            let inAddr = dataPtr + offsetOf__sin_addr__in__sockaddr_in()
+            let result = descriptionData.withUnsafeMutableBytes { (descriptionPtr: UnsafeMutablePointer<Int8>) in
+                inet_ntop(AF_INET, inAddr, descriptionPtr, socklen_t(descriptionData.count))
+            }
+            guard result != nil else { return nil }
+            return String(data: descriptionData, encoding: .utf8)
         }
     }
     
     fileprivate var inPortDescription: String? {
-        let pointer = (data as NSData).bytes.bindMemory(to: sockaddr_in.self, capacity: data.count)
         switch inFamily {
-        case sa_family_t(AF_INET6):
-            fallthrough
-        case sa_family_t(AF_INET):
-            return "\(CFSwapInt16BigToHost(UInt16(pointer.pointee.sin_port)))"
+        case sa_family_t(AF_INET6), sa_family_t(AF_INET):
+            return data.withUnsafeBytes { (dataPtr: UnsafePointer<sockaddr_in>) in
+                "\(CFSwapInt16BigToHost(UInt16(dataPtr.pointee.sin_port)))"
+            }
         default:
             return nil
         }
